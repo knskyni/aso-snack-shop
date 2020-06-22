b4f865bcb487e6a52bbc715303cffb0a60b02262
shirouzujunya <aso1801165@gmail.com>
2020-06-22 13:02:31 +0900

    feat:ItemBeanを作成

---
+++ src/snack/bean/ItemBean.java
@@ -0,0 +1,87 @@
+package snack.bean;
+
+import java.util.Date;
+
+public class ItemBean {
+
+    private int id;
+
+    private String name;
+
+    private String description;
+
+    private boolean status;
+
+    private Date createdAt;
+
+    private Date updateAt;
+
+    private int price;
+
+    private int count;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(Date updateAt) {
+        this.updateAt = updateAt;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+}
