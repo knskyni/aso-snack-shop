7f968a4fda9b8acd8a4b3879dc43835f40523760
knskyni <knskyni@yanai-k.com>
2020-06-30 14:12:13 +0900

    feat: お知らせ作成の入力受け取りを実装

--- src/snack/controller/NewsCreateInputServlet.java
+++ src/snack/controller/NewsCreateInputServlet.java
@@ -8,6 +8,11 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.commons.text.StringEscapeUtils;
+
+import snack.bean.NewsBean;
 
 @WebServlet("/news/create")
 public class NewsCreateInputServlet extends HttpServlet {
@@ -17,6 +22,36 @@ public class NewsCreateInputServlet extends HttpServlet {
 
         RequestDispatcher rd = request.getRequestDispatcher("../WEB-INF/jsp/news/create_input.jsp");
         rd.forward(request, response);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        // フォームから値の受け取り
+        String subject = request.getParameter("subject");
+        String content = request.getParameter("content");
+
+        // 値が空で400エラー
+        if(subject.isEmpty() || content.isEmpty()) {
+            response.sendError(400);
+        }
+
+        // 文字数オーバーで414エラー
+        if(subject.length() > 32 || content.length() > 4096) {
+            response.sendError(414);
+        }
+
+        // 入力内容をBeanに格納
+        NewsBean news = new NewsBean();
+        news.setSubject(StringEscapeUtils.escapeHtml4(subject));
+        news.setContent(content);
+
+        // 入力内容をセッションに格納
+        HttpSession session = request.getSession(false);
+        session.setAttribute("news", news);
+
+        response.sendRedirect("confirm");
 
     }
 
