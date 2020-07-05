468291409e5494c422fc864f2f8bc66af46e9dfb
yanakence <yanakence@yanah.jp>
2020-07-05 23:56:51 +0900

    feat: エラーメッセージを追加する関数を実装

--- src/snack/helper/ErrorHelper.java
+++ src/snack/helper/ErrorHelper.java
@@ -1,5 +1,34 @@
 package snack.helper;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class ErrorHelper {
+
+    public static HashMap<String, ArrayList<String>> add(HashMap<String, ArrayList<String>> errors, String key, String message) {
+
+        if(errors == null) {
+            errors = new HashMap<String, ArrayList<String>>();
+        }
+
+        // キー（エラー項目）がすでに存在するなら
+        if(errors.containsKey(key)) {
+            ArrayList<String> messages = errors.get(key); //
+
+            if(messages == null) {
+                messages = new ArrayList<String>();
+            }
+
+            messages.add(message);
+            errors.put(key, messages);
+        } else {
+            ArrayList<String> messages = new ArrayList<String>();
+            messages.add(message);
+            errors.put(key, messages);
+        }
+
+        return errors;
+
+    }
 
 }
