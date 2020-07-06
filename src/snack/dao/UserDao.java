e5456b9fc4c4deb9a1af94d5bbebebc144bb7fbf
mkiUma <beehivetype01@gmail.com>
2020-07-06 10:49:48 +0900

    change: ログイン時のBean内容を減らした #6

--- src/snack/dao/UserDao.java
+++ src/snack/dao/UserDao.java
@@ -8,7 +8,7 @@ import snack.bean.UserBean;
 public class UserDao extends DaoBase{
 
     public UserBean auth(String email,String password) {
-        if(con == null ) {
+        if(con == null) {
             return null;
         }
 
@@ -26,16 +26,8 @@ public class UserDao extends DaoBase{
             while(rs.next()) {
                 userbean = new UserBean();
 
-                userbean.setId(rs.getInt("Id"));
-                userbean.setLastName(rs.getString("last_name"));
-                userbean.setFirstName(rs.getString("first_name"));
-                userbean.setLastNameFurigana(rs.getString("last_name_furigana"));
-                userbean.setFirstNameFurigana(rs.getString("first_name_furigana"));
-                userbean.setEmail(rs.getString("email"));
-                userbean.setPassword(rs.getString("password"));
+                userbean.setId(rs.getInt("id"));
                 userbean.setType(rs.getString("type"));
-                userbean.setPostalCode(rs.getString("address"));
-                userbean.setPhoneNumber(rs.getString("Number"));
             }
         }catch(Exception e) {
             e.printStackTrace();
