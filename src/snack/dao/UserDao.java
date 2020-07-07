3e7f60c69ae036fa79f274011b61e0bdd54ce2cc
shirouzu_junya <aso1801165@gmail.com>
2020-07-07 12:48:24 +0900

    feat:authAdminを実装

--- src/snack/dao/UserDao.java
+++ src/snack/dao/UserDao.java
@@ -35,5 +35,31 @@ public class UserDao extends DaoBase{
         return userbean;
 
     }
+    public UserBean authAdmin(String email,String password) {
+        if( con == null ){
+            return null;
+        }
 
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        UserBean userBean = new UserBean();
+
+
+        try{
+            stmt = con.prepareStatement("SELECT id, email FROM admins WHERE email=? AND password=?");
+            stmt.setString(1, email);
+            stmt.setString(2, password);
+            rs = stmt.executeQuery();
+            System.out.println(stmt);
+
+            while( rs.next() ) {
+                userBean.setId(rs.getInt("id"));
+                userBean.setEmail(rs.getString("email"));
+            }
+
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+        return userBean;
+    }
 }
