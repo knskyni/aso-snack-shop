3b483c22eb2d0394ddfa23b9ae96a4bc896ed5df
sou <sou20000129@gmail.com>
2020-07-10 11:54:51 +0900

    feat:画面遷移を作成

--- src/snack/controller/UserCreateInputServlet.java
+++ src/snack/controller/UserCreateInputServlet.java
@@ -8,6 +8,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import snack.bean.UserBean;
 
 /**
  * Servlet implementation class UserCreateInputServlet
@@ -15,7 +18,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/user/input")
 public class UserCreateInputServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -36,7 +39,34 @@ public class UserCreateInputServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	    // フォームから値の受け取り
+        String Ksei = request.getParameter("Ksei");
+        String Kmei = request.getParameter("Kmei");
+        String Hsei= request.getParameter("Hsei");
+        String Hmei = request.getParameter("Hmei");
+        String mail = request.getParameter("mail");
+        String pass = request.getParameter("pass");
+        String Kpass = request.getParameter("Kpass");
+        String post = request.getParameter("post");
+        String address = request.getParameter("address");
+        String namber = request.getParameter("namber");
+
+        // 入力内容をBeanに格納
+        UserBean user = new UserBean();
+        user.setFirstName(Ksei);
+        user.setLastName(Kmei);
+        user.setFirstNameFurigana(Hsei);
+        user.setLastNameFurigana(Hmei);
+        user.setEmail(mail);
+        user.setPassword(pass);
+        user.setPostalCode(post);
+        user.setAddress(address);
+        user.setPhoneNumber(namber);
+
+        HttpSession session = request.getSession(false);
+        session.setAttribute("user", user);
+
+	    response.sendRedirect("confirm");
 	}
 
 }
