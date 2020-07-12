bd24feae2d07be7db7a330f2af27045cc318713f
knskyni <knskyni@yanai-k.com>
2020-07-12 19:23:20 +0900

    fix: HTTP / HTTPSの両方に対応できるようにした

--- src/snack/helper/WebHelper.java
+++ src/snack/helper/WebHelper.java
@@ -4,6 +4,6 @@ import javax.servlet.http.HttpServletRequest;
 
 public class WebHelper {
     public static String getRootURL(HttpServletRequest request) {
-        return "http://" + request.getServerName() + ":" + request.getServerPort() + request.getContextPath();
+        return "//" + request.getServerName() + ":" + request.getServerPort() + request.getContextPath();
     }
 }
