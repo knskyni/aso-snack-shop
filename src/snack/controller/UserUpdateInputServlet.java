88c6146a29774f6633bf4f55cbc97ba1c6cc40f0
knskyni <knskyni@yanai-k.com>
2020-07-14 11:08:32 +0900

    refactor: 整形

--- src/snack/controller/UserUpdateInputServlet.java
+++ src/snack/controller/UserUpdateInputServlet.java
@@ -1,8 +1,6 @@
 package snack.controller;
 
 import java.io.IOException;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -22,8 +20,7 @@ import snack.model.UserModel;
 public class UserUpdateInputServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         HttpSession session = request.getSession(true);
         UserBean userInfoBean = (UserBean)session.getAttribute("userInfo");
@@ -54,115 +51,71 @@ public class UserUpdateInputServlet extends HttpServlet {
         String address = request.getParameter("address");
         String phoneNumber = request.getParameter("phoneNumber");
 
-        //現在日時を取得
-
-
-        LocalDateTime date1 = LocalDateTime.now();
-
-        DateTimeFormatter dtformat = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm");
-        String fdate1 = dtformat.format(date1);
-
-
-
         HashMap<String, ArrayList<String>> errors = new HashMap<String, ArrayList<String>>();
 
         if(lastName.isEmpty()) {
             errors = ErrorHelper.add(errors, "lastName", "*姓を入力してください");
         }
 
-
-
         if(firstName.isEmpty()) {
             errors = ErrorHelper.add(errors, "firstName", "*名を入力してください");
         }
-
-
 
         if(lastNameFurigana.isEmpty() || lastNameFurigana.matches("^[\\u30A0-\\u30FF]+$")) {
             errors = ErrorHelper.add(errors, "lastNameFurigana", "*せいをひらがなで入力してください");
         }
 
-
-
         if(firstNameFurigana.isEmpty() || firstNameFurigana.matches("^[\\u30A0-\\u30FF]+$")) {
             errors = ErrorHelper.add(errors, "firstNameFurigana", "*めいをひらがなで入力してください");
         }
-
-
 
         if(email.isEmpty()) {
             errors = ErrorHelper.add(errors, "email", "*正しく入力してください");
         }
 
-
-
         if(postalNumber.isEmpty() || !postalNumber.matches("[0-9a-zA-Z\\-\\_]+" )){
             errors = ErrorHelper.add(errors, "postalNumber", "*ハイフンを使って正しく入力してください");
         }
-
-
 
         if(address.isEmpty()) {
             errors = ErrorHelper.add(errors, "address", "*住所を入力してください");
         }
 
-
-
-        if(phoneNumber.isEmpty() ||!phoneNumber.matches("[0-9a-zA-Z\\-\\_]+")) {
+        if(phoneNumber.isEmpty() || !phoneNumber.matches("[0-9a-zA-Z\\-\\_]+")) {
             errors = ErrorHelper.add(errors, "phoneNumber", "*ハイフンを使って電話番号を入力してください");
         }
-
-
 
         if(lastName.length() > 16) {
             errors = ErrorHelper.add(errors, "lastName", "*16文字以内で入力してください");
         }
 
-
-
         if(firstName.length() > 32) {
             errors = ErrorHelper.add(errors, "firstName", "*32文字以内で入力してください");
         }
-
-
 
         if(lastNameFurigana.length() > 32) {
             errors = ErrorHelper.add(errors, "lastNameFurigana", "*32文字以内で入力してください");
         }
 
-
-
         if(firstNameFurigana.length() > 16) {
             errors = ErrorHelper.add(errors, "lastNameFurigana", "*16文字以内で入力してください");
         }
-
-
 
         if(email.length() > 128) {
             errors = ErrorHelper.add(errors, "email", "*128文字以内で入力してください");
         }
 
-
-
         if(postalNumber.length() > 8) {
             errors = ErrorHelper.add(errors, "postalNumber", "*16文字以内で入力してください");
         }
-
-
 
         if(address.length() > 128) {
             errors = ErrorHelper.add(errors, "address", "*128文字以内で入力してください");
         }
 
-
-
         if(phoneNumber.length() > 13) {
             errors = ErrorHelper.add(errors, "phoneNumber", "*13文字以内で入力してください");
         }
-
-
-
-
 
         HttpSession session = request.getSession(true);
         UserBean userIdInfo = (UserBean)session.getAttribute("userIdInfo");
@@ -178,20 +131,16 @@ public class UserUpdateInputServlet extends HttpServlet {
         userUpdateInfo.setAddress(address);
         userUpdateInfo.setPhoneNumber(phoneNumber);
 
-
         session.setAttribute("userInfo", userUpdateInfo);
 
         if(errors.isEmpty()) {
             UserModel userModel = new UserModel();
             userModel.update(userUpdateInfo);
             response.sendRedirect("/acount/show");
-
-
-        }else {
+        } else {
             request.setAttribute("errors", errors);
             RequestDispatcher rd = request.getRequestDispatcher("../WEB-INF/jsp/user/update_input.jsp");
             rd.forward(request, response);
         }
-
     }
 }
