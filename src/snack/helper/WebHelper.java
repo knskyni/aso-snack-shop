a72bbdf84eaaf2b47c1e0b6a9797978c7e363678
yanakence <yanakence@yanah.jp>
2020-07-15 11:19:15 +0900

    feat: ファイルの保存部分を実装

--- src/snack/helper/WebHelper.java
+++ src/snack/helper/WebHelper.java
@@ -1,6 +1,7 @@
 package snack.helper;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -9,6 +10,8 @@ import java.util.stream.Collectors;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.Part;
+
+import org.apache.commons.codec.digest.DigestUtils;
 
 public class WebHelper {
     public static String getRootURL(HttpServletRequest request) {
@@ -30,5 +33,31 @@ public class WebHelper {
         }
 
         return param;
+    }
+
+    public static String saveFileFromPart(HttpServletRequest request, String name, String path) throws ServletException, IOException {
+        Part part = request.getPart(name);
+
+        // ディレクトリ生成
+        File file = new File(path);
+        if(!file.exists()) {
+            file.mkdirs();
+        }
+
+        // ファイルデータのハッシュ値取得（ファイル名に使用）
+        String fileName = DigestUtils.md5Hex(part.getInputStream());
+
+        // 拡張子の取得
+        for(String line : part.getHeader("Content-Disposition").split(";")) {
+            line = line.trim();
+            if(line.startsWith("filename")) {
+                fileName += line.substring(line.indexOf(".")).replaceAll("\"", "");
+            }
+        }
+
+        // ファイルの保存
+        part.write(path + fileName);
+
+        return fileName;
     }
 }
