6c389ec898e6d7826baa8dc026c3a354959f4057
knskyni <knskyni@yanai-k.com>
2020-07-17 12:22:09 +0900

    feat: 商品新規作成のDB登録を実装

--- src/snack/model/ItemModel.java
+++ src/snack/model/ItemModel.java
@@ -1,5 +1,22 @@
 package snack.model;
 
-public class ItemModel {
+import snack.bean.ItemBean;
+import snack.dao.ItemDao;
 
+public class ItemModel {
+    public boolean create(ItemBean item) {
+        ItemDao itemDao = new ItemDao();
+        boolean result = false;
+
+        try {
+            itemDao.connect();
+            result = itemDao.create(item);
+        } catch(Exception e) {
+            e.printStackTrace();
+        } finally {
+            itemDao.close();
+        }
+
+        return result;
+    }
 }
