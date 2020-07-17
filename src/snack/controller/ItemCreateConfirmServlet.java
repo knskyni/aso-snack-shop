f15fb28cc3bced42197f236e137c68b36fed4217
knskyni <knskyni@yanai-k.com>
2020-07-17 14:19:48 +0900

    feat: 商品追加確認画面を実装

---
+++ src/snack/controller/ItemCreateConfirmServlet.java
@@ -0,0 +1,34 @@
+package snack.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import snack.bean.ItemBean;
+
+@WebServlet("/item/create/confirm")
+public class ItemCreateConfirmServlet extends HttpServlet {
+    private static final String jsp = "../../WEB-INF/jsp/item/create_confirm.jsp";
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // セッション
+        HttpSession session = request.getSession(false);
+        ItemBean news = (ItemBean)session.getAttribute("createItem");
+
+        // Beanの存在確認
+        if(news == null) {
+            response.sendError(400);
+            return;
+        }
+
+        // JSP表示
+        RequestDispatcher rd = request.getRequestDispatcher(jsp);
+        rd.forward(request, response);
+    }
+}
