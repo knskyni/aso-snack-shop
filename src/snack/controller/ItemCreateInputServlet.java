de9f6491420b457146344f1fa9deba2ab9e7aca6
knskyni <knskyni@yanai-k.com>
2020-07-17 14:08:12 +0900

    feat: 商品新規作成のフォーム受け取りを実装

--- src/snack/controller/ItemCreateInputServlet.java
+++ src/snack/controller/ItemCreateInputServlet.java
@@ -1,14 +1,25 @@
 package snack.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import snack.bean.ItemBean;
+import snack.exception.FormFileEmptyException;
+import snack.helper.ErrorHelper;
+import snack.helper.FormHelper;
+import snack.helper.WebHelper;
+
+@MultipartConfig(location="")
 @WebServlet("/item/create/input")
 public class ItemCreateInputServlet extends HttpServlet {
     private static final String jsp = "../../WEB-INF/jsp/item/create_input.jsp";
@@ -17,5 +28,52 @@ public class ItemCreateInputServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         RequestDispatcher rd = request.getRequestDispatcher(jsp);
         rd.forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // エラーメッセージ
+        HashMap<String, ArrayList<String>> errors = new HashMap<String, ArrayList<String>>();
+
+        // Formから取得
+        String name = WebHelper.getStrParamFromPart(request, "name");
+        String description = WebHelper.getStrParamFromPart(request, "description");
+        String imagePath = "";
+        int price = 0;
+
+        try {
+            imagePath = WebHelper.saveFileFromPart(request, "image", getServletContext().getRealPath("img/item/"));
+        } catch(FormFileEmptyException e) {
+            errors = ErrorHelper.add(errors, "image", "画像ファイルが選択されていません。");
+        }
+
+        try {
+            price = Integer.parseInt(WebHelper.getStrParamFromPart(request, "price"));
+        } catch(NumberFormatException e) {
+            errors = ErrorHelper.add(errors, "price", "価格が正確な値ではありません。");
+        }
+
+        errors = FormHelper.checkEmpty(errors, "name", name);
+        errors = FormHelper.checkEmpty(errors, "description", description);
+
+        // Beanに格納
+        ItemBean item = new ItemBean();
+        item.setName(name);
+        item.setDescription(description);
+        item.setImagePath(imagePath);
+        item.setPrice(price);
+
+        // セッションに格納
+        HttpSession session = request.getSession(false);
+        session.setAttribute("createItem", item);
+
+        // エラーの存在によって遷移先の切り替え
+        if(errors.isEmpty()) {
+            response.sendRedirect("confirm");
+        } else {
+            request.setAttribute("errors", errors);
+            RequestDispatcher rd = request.getRequestDispatcher(jsp);
+            rd.forward(request, response);
+        }
     }
 }
