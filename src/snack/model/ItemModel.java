6eb8c8fe8f64dd284197777fa6454da810774644
yanakence <yanakence@yanah.jp>
2020-07-18 19:12:40 +0900

    feat: データベース処理を実装

--- src/snack/model/ItemModel.java
+++ src/snack/model/ItemModel.java
@@ -19,4 +19,54 @@ public class ItemModel {
 
         return result;
     }
+
+    public ItemBean show(int id) {
+        ItemBean itemBean = null;
+        ItemDao itemDao = new ItemDao();
+
+        try {
+            itemDao.connect();
+            itemBean = itemDao.show(id);
+        } catch(Exception e) {
+            e.printStackTrace();
+        } finally {
+            itemDao.close();
+        }
+
+        return itemBean;
+    }
+
+    public boolean update(ItemBean item) {
+        ItemDao itemDao = new ItemDao();
+        boolean result = false;
+
+        try {
+            itemDao.connect();
+            result = itemDao.update(item);
+        } catch(Exception e) {
+            e.printStackTrace();
+            result = false;
+        } finally {
+            itemDao.close();
+        }
+
+        return result;
+    }
+
+    public boolean delete(int id) {
+        ItemDao itemDao = new ItemDao();
+        boolean result = false;
+
+        try {
+            itemDao.connect();
+            result = itemDao.delete(id);
+        } catch(Exception e) {
+            e.printStackTrace();
+            result = false;
+        } finally {
+            itemDao.close();
+        }
+
+        return result;
+    }
 }
