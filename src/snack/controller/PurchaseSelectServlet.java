bd6c44fb909888260dd6bd364c3150c59f96d81e
mkiUma <beehivetype01@gmail.com>
2020-07-24 14:45:17 +0900

    feat: 購入方法選択機能を実装

--- src/snack/controller/PurchaseSelectServlet.java
+++ src/snack/controller/PurchaseSelectServlet.java
@@ -8,6 +8,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import snack.bean.UserBean;
 
 @WebServlet("/purchase/select")
 public class PurchaseSelectServlet extends HttpServlet {
@@ -15,36 +18,37 @@ public class PurchaseSelectServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        HttpSession session = request.getSession(true);
+        UserBean userAuth = (UserBean)session.getAttribute("userAuth");
+        if(userAuth == null) {
+            response.sendError(404);
+        }else {
         RequestDispatcher dispatcher = request.getRequestDispatcher("../WEB-INF/jsp/purchase/select.jsp");
         dispatcher.forward(request, response);
 
+        }
     }
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         String select = request.getParameter("selectRadios");
-        String card = request.getParameter("cardNumber");
+        String cardNum = request.getParameter("cardNumber");
 
-        if(select == null) {
+        if(select == "card" && cardNum.equals("")) {
+            String msg = "カード番号を入力してください";
+            request.setAttribute("msg", msg);
+
+            RequestDispatcher dispatcher = request.getRequestDispatcher("../WEB-INF/jsp/purchase/select.jsp");
+            dispatcher.forward(request, response);
+        }else if(select == null) {
             String msg = "＊決済方法を選択してください";
 
             request.setAttribute("msg", msg);
 
-            RequestDispatcher dispatcher = request.getRequestDispatcher("./WEB-INF/jsp/user/select.jsp");
+            RequestDispatcher dispatcher = request.getRequestDispatcher("../WEB-INF/jsp/purchase/select.jsp");
             dispatcher.forward(request, response);
-        } else {
-
-            if(select == "card" && card == null) {
-                String msg = "カード番号を入力してください";
-
-                request.setAttribute("msg", msg);
-
-                RequestDispatcher dispatcher = request.getRequestDispatcher("./WEB-INF/jsp/user/select.jsp");
-                dispatcher.forward(request, response);
-            } else {
-                response.sendRedirect("comfirm");
-
-            }
+        }else {
+            response.sendRedirect("comfirm");
         }
     }
 }
