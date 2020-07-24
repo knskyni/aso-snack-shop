3db5abe3b928a8e86eb4580447e526f4021375d6
mkiUma <beehivetype01@gmail.com>
2020-07-24 14:53:50 +0900

    feat: 購入処理を実装

--- src/snack/model/PurchaseModel.java
+++ src/snack/model/PurchaseModel.java
@@ -1,5 +1,22 @@
 package snack.model;
 
-public class PurchaseModel {
+import snack.bean.PurchaseBean;
+import snack.dao.PurchaseDao;
 
+public class PurchaseModel {
+    public boolean execute(PurchaseBean purchaseBean) {
+        PurchaseDao purchaseDao = new PurchaseDao();
+        boolean result = false;
+
+        try {
+            purchaseDao.connect();
+            result = purchaseDao.execute(purchaseBean);
+        } catch(Exception e) {
+            e.printStackTrace();
+        } finally {
+            purchaseDao.close();
+        }
+
+        return result;
+    }
 }
