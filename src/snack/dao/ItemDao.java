51ca98213a4dc68b63ff47b6538611d714386c84
gondo <1801163@s.aso.juku.ac.jp>
2020-07-28 11:52:10 +0900

    feat.カートに商品を追加する機能

--- src/snack/dao/ItemDao.java
+++ src/snack/dao/ItemDao.java
@@ -1,5 +1,48 @@
 package snack.dao;
 
-public class ItemDao {
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+import snack.bean.ItemBean;
+
+public class ItemDao extends DaoBase{
+
+    //カート商品追加
+    public List<ItemBean> getList(int Id){
+
+        if( con == null ) {
+            return null;
+        }
+        List<ItemBean> Cart = new ArrayList<ItemBean>();
+
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+
+        try {
+            stmt = con.prepareStatement("SELECT * FROM items WHERE id=?");
+            stmt.setInt(1,Id);
+            rs = stmt.executeQuery();
+
+            while( rs.next() ) {
+                ItemBean listCart = new ItemBean();
+
+                listCart.setId(rs.getInt("id"));
+                listCart.setName(rs.getString("name"));
+                listCart.setDescription(rs.getString("description"));
+                listCart.setImage(rs.getString("image_path"));
+                listCart.setPrice(rs.getInt("price"));
+
+                Cart.add(listCart);
+            }
+
+        }catch(SQLException e) {
+            //エラー発生した場合にコンソールにログを出力する
+            e.printStackTrace();
+        }
+
+                return Cart;
+    }
 }
