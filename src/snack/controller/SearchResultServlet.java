75bb3b61b9f8c13c5f2586e0eddde5e0c2754074
HiraishiShouki <hiraisi0117@gmail.com>
2020-08-03 17:59:46 +0900

    fiat:検索

--- src/snack/controller/SearchResultServlet.java
+++ src/snack/controller/SearchResultServlet.java
@@ -1,5 +1,6 @@
 package snack.controller;
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -8,12 +9,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import snack.bean.ItemBean;
+import snack.bean.NewsBean;
+import snack.model.ItemModel;
+import snack.model.NewsModel;
+
 @WebServlet("/search")
 public class SearchResultServlet extends HttpServlet{
     @Override
     protected void doGet(HttpServletRequest request,HttpServletResponse response)
             throws ServletException, IOException{
-        /*
+
         String search = request.getParameter("ItemName");
 
         // データベースから情報取得
@@ -27,7 +33,7 @@ public class SearchResultServlet extends HttpServlet{
            NewsModel newsModel = new NewsModel();
            List<NewsBean> news = newsModel.list();
            request.setAttribute("news", news);
-*/
+
         RequestDispatcher dispatcher =
                 request.getRequestDispatcher("../WEB-INF/jsp/admin/login.jsp");
         dispatcher.forward(request, response);
