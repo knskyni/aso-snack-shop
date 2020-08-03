763368ff0241692682fa5f0d5ad8898f2f903433
HiraishiShouki <hiraisi0117@gmail.com>
2020-08-03 17:42:23 +0900

    feat:検索

--- src/snack/controller/SearchResultServlet.java
+++ src/snack/controller/SearchResultServlet.java
@@ -1,6 +1,5 @@
 package snack.controller;
 import java.io.IOException;
-import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,33 +8,28 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import snack.bean.ItemBean;
-import snack.bean.NewsBean;
-import snack.model.ItemModel;
-import snack.model.NewsModel;
-
 @WebServlet("/search")
 public class SearchResultServlet extends HttpServlet{
     @Override
     protected void doGet(HttpServletRequest request,HttpServletResponse response)
             throws ServletException, IOException{
-
+        /*
         String search = request.getParameter("ItemName");
 
-     // データベースから情報取得
-        ItemModel itemModel = new ItemModel();
+        // データベースから情報取得
+           ItemModel itemModel = new ItemModel();
 
-        List<ItemBean> itemBean = itemModel.getList(search);
+           List<ItemBean> itemBean = itemModel.getList(search);
 
 
-        request.setAttribute("itemBean", itemBean);
+           request.setAttribute("itemBean", itemBean);
 
-        NewsModel newsModel = new NewsModel();
-        List<NewsBean> news = newsModel.list();
-        request.setAttribute("news", news);
-
-        RequestDispatcher dispatcher = request.getRequestDispatcher("../WEB-INF/jsp/searchresult/searchresult.jsp");
+           NewsModel newsModel = new NewsModel();
+           List<NewsBean> news = newsModel.list();
+           request.setAttribute("news", news);
+*/
+        RequestDispatcher dispatcher =
+                request.getRequestDispatcher("../WEB-INF/jsp/admin/login.jsp");
         dispatcher.forward(request, response);
-
     }
 }
