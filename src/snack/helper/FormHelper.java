2cba0d1a2b544c2ab8b52de239d713951bed684e
knskyni <knskyni@yanai-k.com>
2020-08-06 10:04:39 +0900

    feat: 文字列のチェックを実装

--- src/snack/helper/FormHelper.java
+++ src/snack/helper/FormHelper.java
@@ -12,4 +12,13 @@ public class FormHelper {
 
         return false;
     }
+
+    public static boolean checkLength(String str, int min, int max) {
+        int length = str.length();
+        if(length >= min && length <= max) {
+            return false;
+        }
+
+        return true;
+    }
 }
