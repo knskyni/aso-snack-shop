2f61005e66f57369b9e79854c006f2f530a06c17
yanakence <yanakence@yanah.jp>
2020-08-06 10:05:20 +0900

    refactor: コーディング整形

--- src/snack/helper/FormHelper.java
+++ src/snack/helper/FormHelper.java
@@ -2,22 +2,15 @@ package snack.helper;
 
 public class FormHelper {
     public static boolean checkEmpty(String str) {
-        if(str == null) {
-            return true;
-        }
-
-        if(str.replaceAll(" ", "").equals("")) {
-            return true;
-        }
+        if(str == null) return true;
+        if(str.replaceAll(" ", "").equals("")) return true;
 
         return false;
     }
 
     public static boolean checkLength(String str, int min, int max) {
         int length = str.length();
-        if(length >= min && length <= max) {
-            return false;
-        }
+        if(length >= min && length <= max) return false;
 
         return true;
     }
