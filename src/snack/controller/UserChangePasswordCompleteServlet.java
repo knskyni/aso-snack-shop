45e3e0d44c53b0986dda1daa6135db5abb23ebd8
knskyni <knskyni@yanai-k.com>
2020-08-19 23:22:29 +0900

    feat: パスワード変更完了後にログアウトするようにした

--- src/snack/controller/UserChangePasswordCompleteServlet.java
+++ src/snack/controller/UserChangePasswordCompleteServlet.java
@@ -8,12 +8,15 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/account/change-password/complete")
 public class UserChangePasswordCompleteServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession(true);
+        session.invalidate();
 
         RequestDispatcher dispatcher = request.getRequestDispatcher("../../WEB-INF/jsp/user/password_complete.jsp");
         dispatcher.forward(request,response);
